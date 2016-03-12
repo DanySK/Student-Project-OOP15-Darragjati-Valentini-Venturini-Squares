825ba98f9ed0f513d43e8b3a11f73ec855824fb3
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-03-12 22:51:40 +0100

    aggiunti metodi alla classe player per ottenere dati come totale vittorie, totale partite e winrate

--- src/model/classes/Player.java
+++ src/model/classes/Player.java
@@ -3,17 +3,57 @@ package model.classes;
 public class Player {
  
     private String playerName;
-    Integer points;
+    private Integer score;
+    private Integer win;
+    private Integer totalMatches;
+    private double winRate;
    
     
     public Player(String playerName){
 	
 	this.playerName = playerName;
+	this.score = 0;
+	this.win = 0;
+	this.totalMatches = 0;
+	this.winRate = 0;
     }
     
     public String getPlayerName(){
 	return playerName;
     }
     
+    public Integer getPlayerScore(){
+	return this.score;
+    }
     
+    public Integer getTotalWins(){
+	return this.win;
+    }
+    
+    public Integer getTotalMatches(){
+	return this.totalMatches;
+    }
+    
+    public double getWinRatio() {
+	return this.winRate;
+    }
+    
+    public void addLastMatchResults(Integer score, boolean victory){
+	
+	this.score += score;
+	
+	if(victory){
+	    this.win++;
+	}
+	
+	this.totalMatches++;
+	
+	calculateWinRate();
+	
+    }
+    
+    private void calculateWinRate(){
+	
+	this.winRate = getTotalWins() / getTotalMatches();
+    }
 }
