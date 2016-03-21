9862e1f9b1231bb67ee4d207eb61ec36724a9e3e
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-03-21 11:44:24 +0100

    cambiato il metodo createNewPlayerList e orderListByWinrate in modo che  restituiscano una List<<Pair<String, PlayerData<Double, Integer, Integer, Integer>>> come richiesto dal nuovo updatedRanking

--- src/model/classes/RankingImpl.java
+++ src/model/classes/RankingImpl.java
@@ -1,6 +1,9 @@
 package model.classes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,82 +13,79 @@ import model.interfaces.Ranking;
 public class RankingImpl implements Ranking {
 
     private Map<String, PlayerData<Double, Integer, Integer, Integer>> oldRanking = new HashMap<>();
-    private List<List<String>> updatedRanking = new ArrayList<>();
+    private List<Pair<String, PlayerData<Double, Integer, Integer, Integer>>> updatedRanking = new ArrayList<>();
     private static final Integer WINRATE_CODE = 1;
     private static final Integer WINS_CODE = 2;
     private static final Integer TOTAL_MATCHES_CODE = 3;
     private static final Integer TOTAL_SQUARES_CATCHED_CODE = 4;
-    
-    public RankingImpl( Map<String, PlayerData<Double, Integer, Integer, Integer>> oldRanking) {
-	this.oldRanking = oldRanking;
+
+    public RankingImpl(Map<String, PlayerData<Double, Integer, Integer, Integer>> oldRanking) {
+        this.oldRanking = oldRanking;
     }
-    
+
     @Override
     public void addPlayerResults(String playerName, boolean win, Integer score) {
 
-	if (oldRanking.containsKey(playerName)) {
-		updatePlayerResults(playerName, win, score);
-		return;
-	    }
-	
-	addNewPlayer(playerName, win, score);
+        if (oldRanking.containsKey(playerName)) {
+            updatePlayerResults(playerName, win, score);
+            return;
+        }
+
+        addNewPlayer(playerName, win, score);
     }
 
     private void addNewPlayer(String playerName, boolean win, Integer score) {
 
-	PlayerData<Double, Integer, Integer, Integer> playerScores = new PlayerData<>();
-	if(win){
-	    playerScores.setTotalWins(1);
-	}
-	playerScores.setTotalMatches(1);
-	playerScores.setWinrate((double) (playerScores.getTotalWins()/playerScores.getTotalMatches()));
-	playerScores.setSquareCatched(score);
-	oldRanking.put(playerName, playerScores);
+        PlayerData<Double, Integer, Integer, Integer> playerScores = new PlayerData<>();
+        if (win) {
+            playerScores.setTotalWins(1);
+        }
+        playerScores.setTotalMatches(1);
+        playerScores.setWinrate((double) (playerScores.getTotalWins() / playerScores.getTotalMatches()));
+        playerScores.setSquareCatched(score);
+        oldRanking.put(playerName, playerScores);
     }
 
     private void updatePlayerResults(String playerName, boolean win, Integer score) {
 
-	PlayerData<Double, Integer, Integer, Integer> playerScores = new PlayerData<>();
-	if(win){
-	    playerScores.setTotalWins(oldRanking.get(playerName).getTotalWins()+1);
-	}
-	playerScores.setTotalMatches(oldRanking.get(playerName).getTotalMatches()+1);
-	playerScores.setWinrate((double) (playerScores.getTotalWins()/playerScores.getTotalMatches()));
-	playerScores.setSquareCatched(oldRanking.get(playerName).getSquareCatched()+score);
-	oldRanking.replace(playerName, playerScores);
+        PlayerData<Double, Integer, Integer, Integer> playerScores = new PlayerData<>();
+        if (win) {
+            playerScores.setTotalWins(oldRanking.get(playerName).getTotalWins() + 1);
+        }
+        playerScores.setTotalMatches(oldRanking.get(playerName).getTotalMatches() + 1);
+        playerScores.setWinrate((double) (playerScores.getTotalWins() / playerScores.getTotalMatches()));
+        playerScores.setSquareCatched(oldRanking.get(playerName).getSquareCatched() + score);
+        oldRanking.replace(playerName, playerScores);
     }
-    
-    public List<List<String>> orderListByWinrate(){
-        
-        for(String player : oldRanking.keySet()){
-            
-            if(updatedRanking.size()==0){
+
+    public List<Pair<String, PlayerData<Double, Integer, Integer, Integer>>> orderListByWinrate() {
+
+        for (String player : oldRanking.keySet()) {
+
+            if (updatedRanking.size() == 0) {
                 updatedRanking.add(createNewPlayerList(player));
-            }
-            else{
-                for(int i = 0; i < updatedRanking.size(); i++){
-                    if(oldRanking.get(player).getWinRate() < Integer.parseInt(updatedRanking.get(i).get(WINRATE_CODE))){
+            } else {
+                for (int i = 0; i < updatedRanking.size(); i++) {
+                    if (oldRanking.get(player).getWinRate() > updatedRanking.get(i).getY().getWinRate()) {
                         updatedRanking.add(i, createNewPlayerList(player));
                         break;
                     }
                 }
-                
+
             }
         }
-         
-        //mettere copyof
-        return updatedRanking;
+
+        return Collections.unmodifiableList(updatedRanking);      
     }
 
-    private List<String> createNewPlayerList(String player){
-        
-        List<String> newPlayer = new ArrayList<>();
-        newPlayer.add(player);
-        newPlayer.add("" + oldRanking.get(player).getWinRate());
-        newPlayer.add("" + oldRanking.get(player).getTotalWins());
-        newPlayer.add("" + oldRanking.get(player).getTotalMatches());
-        newPlayer.add("" + oldRanking.get(player).getSquareCatched());
-        
-        return newPlayer;
+    private Pair<String, PlayerData<Double, Integer, Integer, Integer>> createNewPlayerList(String player) {
+
+        PlayerData<Double, Integer, Integer, Integer> datas = new PlayerData<>();
+        datas.setWinrate(oldRanking.get(player).getWinRate());
+        datas.setTotalWins(oldRanking.get(player).getTotalWins());
+        datas.setTotalMatches(oldRanking.get(player).getTotalMatches());
+        datas.setSquareCatched(oldRanking.get(player).getSquareCatched());
+
+        return new Pair<>(player, datas);
     }
 }
