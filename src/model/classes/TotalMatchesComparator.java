56cd44337ebefb6863cd76ba683e5984fb95f66b
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-03-23 16:05:42 +0100

    aggiunta la classe TotalMatchesComparator che ordina la classifica per numero di partite giocate

---
+++ src/model/classes/TotalMatchesComparator.java
@@ -0,0 +1,30 @@
+package model.classes;
+
+import java.util.Comparator;
+
+public class TotalMatchesComparator implements Comparator<Player>{
+
+    @Override
+    public int compare(Player player1, Player player2) {
+
+        if (player1.getTotalMatches() == player2.getTotalMatches()) {
+
+            if (player1.getWinRate() == player2.getWinRate()) {
+
+                if (player1.getTotalSquaresCatched() == player2.getTotalSquaresCatched()) {
+                    
+                    return player1.getPlayerName().compareTo(player2.getPlayerName());
+                } else {
+                    
+                    return (player1.getTotalSquaresCatched() > player2.getTotalSquaresCatched()) ? 1 : -1;
+                }
+            } else {
+                
+                return (player1.getWinRate() > player2.getWinRate()) ? 1 : -1;
+            }
+        } else{
+            
+            return (player1.getTotalMatches() > player2.getTotalMatches()) ? 1 : -1;
+        }
+    }
+}
