2210d8023ea7348ad756991f7bdeb51bb3fde29f
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-03-23 11:58:48 +0100

    aggiunta la classe WinRateComparator al repository

---
+++ src/model/classes/WinRateComparator.java
@@ -0,0 +1,28 @@
+package model.classes;
+
+import java.util.Comparator;
+
+public class WinRateComparator implements Comparator<Player> {
+
+    @Override
+    public int compare(Player player1, Player player2) {
+
+        if (player1.getWinRate() == player2.getWinRate()) {
+
+            if (player1.getTotalMatches() == player2.getTotalMatches()) {
+
+                if (player1.getTotalSquaresCatched() == player2.getTotalSquaresCatched()) {
+                    
+                    return player1.getPlayerName().compareTo(player2.getPlayerName());
+                }
+                
+                return (player1.getTotalSquaresCatched() > player2.getTotalSquaresCatched()) ? 1 : -1;
+            }
+            
+            return (player1.getTotalMatches() > player2.getTotalMatches()) ? 1 : -1;
+        }
+
+        return (player1.getWinRate() > player2.getWinRate()) ? 1 : -1;
+    }
+
+}
