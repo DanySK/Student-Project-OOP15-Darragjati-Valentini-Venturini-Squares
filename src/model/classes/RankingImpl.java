6ea1a7810087cc97c2ab533f632bd439a34d2783
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-03-23 11:41:20 +0100

    modificata la classe RankingImpl aggiungendo un WinRateComparator per ordinare i giocatori in base al loro winrate

--- src/model/classes/RankingImpl.java
+++ src/model/classes/RankingImpl.java
@@ -2,116 +2,54 @@ package model.classes;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import model.enumerations.RankingOption;
 import model.interfaces.Ranking;
 
 public class RankingImpl implements Ranking {
 
-    private List<Player> list = new ArrayList<>();
-    private Map<String, PlayerData<Double, Integer, Integer, Integer>> oldRanking = new HashMap<>();
-    private List<Pair<String, PlayerData<Double, Integer, Integer, Integer>>> updatedRanking;
+    private List<Player> playerList = new ArrayList<>();
 
-    public RankingImpl(Map<String, PlayerData<Double, Integer, Integer, Integer>> oldRanking) {
-        this.oldRanking = oldRanking;
+    public RankingImpl(List<Player> playerList) {
+        this.playerList = playerList;
     }
 
     @Override
-    public void addPlayerResults(String playerName, boolean win, Integer score) {
+    public void addPlayerResults(String playerName, boolean victory, Integer totalSquaresCatched) {
 
-        if (oldRanking.containsKey(playerName)) {
-            updatePlayerResults(playerName, win, score);
-            return;
-        }
-
-        addNewPlayer(playerName, win, score);
-    }
-
-    private void addNewPlayer(String playerName, boolean win, Integer score) {
-
-        PlayerData<Double, Integer, Integer, Integer> playerScores = new PlayerData<>();
-        if (win) {
-            playerScores.setTotalWins(1);
-        }
-        playerScores.setTotalMatches(1);
-        playerScores.setWinrate((double) (playerScores.getTotalWins() / playerScores.getTotalMatches()));
-        playerScores.setTotalSquaresCatched(score);
-        oldRanking.put(playerName, playerScores);
-    }
-
-    private void updatePlayerResults(String playerName, boolean win, Integer score) {
-
-        PlayerData<Double, Integer, Integer, Integer> playerScores = new PlayerData<>();
-        if (win) {
-            playerScores.setTotalWins(oldRanking.get(playerName).getTotalWins() + 1);
-        }
-        playerScores.setTotalMatches(oldRanking.get(playerName).getTotalMatches() + 1);
-        playerScores.setWinrate((double) (playerScores.getTotalWins() / playerScores.getTotalMatches()));
-        playerScores.setTotalSquaresCatched(oldRanking.get(playerName).getTotalSquaresCatched() + score);
-        oldRanking.replace(playerName, playerScores);
-    }
-
-    public List<Pair<String, PlayerData<Double, Integer, Integer, Integer>>> orderListBy(RankingOption option) {
-
-        updatedRanking = new ArrayList<>();
-        for (String player : oldRanking.keySet()) {
-
-            if (updatedRanking.size() == 0) {
-                updatedRanking.add(createNewPlayerList(player));
-            } else {
-                switch (option) {
-                case WINRATE:
-                    for (int i = 0; i < updatedRanking.size(); i++) {
-                        if (oldRanking.get(player).getWinRate() > updatedRanking.get(i).getY().getWinRate()) {
-                            updatedRanking.add(i, createNewPlayerList(player));
-                            break;
-                        }
-                    }
-                    break;
-                case TOTAL_WINS:
-                    for (int i = 0; i < updatedRanking.size(); i++) {
-                        if (oldRanking.get(player).getTotalWins() > updatedRanking.get(i).getY().getTotalWins()) {
-                            updatedRanking.add(i, createNewPlayerList(player));
-                            break;
-                        }
-                    }
-                    break;
-                case TOTAL_MATCHES:
-                    for (int i = 0; i < updatedRanking.size(); i++) {
-                        if (oldRanking.get(player).getTotalMatches() > updatedRanking.get(i).getY().getTotalMatches()) {
-                            updatedRanking.add(i, createNewPlayerList(player));
-                            break;
-                        }
-                    }
-                    break;
-                case TOTAL_SQUARES_CATCHED:
-                    for (int i = 0; i < updatedRanking.size(); i++) {
-                        if (oldRanking.get(player).getTotalSquaresCatched() > updatedRanking.get(i).getY().getTotalSquaresCatched()) {
-                            updatedRanking.add(i, createNewPlayerList(player));
-                            break;
-                        }
-                    }
-                    break;
-                default:
-                    throw new IllegalStateException("There is a bug here.");
-                }
+        for (Player p : playerList) {
+            if (p.getPlayerName().equals(playerName)) {
+                p.addLastMatchResults(victory, totalSquaresCatched);
+                return;
             }
         }
 
-        return Collections.unmodifiableList(updatedRanking);
+        Player newPlayer = new Player(playerName);
+        newPlayer.addLastMatchResults(victory, totalSquaresCatched);
+        playerList.add(newPlayer);
     }
 
-    private Pair<String, PlayerData<Double, Integer, Integer, Integer>> createNewPlayerList(String player) {
+    public List<Player> orderListBy(RankingOption option) {
 
-        PlayerData<Double, Integer, Integer, Integer> datas = new PlayerData<>();
-        datas.setWinrate(oldRanking.get(player).getWinRate());
-        datas.setTotalWins(oldRanking.get(player).getTotalWins());
-        datas.setTotalMatches(oldRanking.get(player).getTotalMatches());
-        datas.setTotalSquaresCatched(oldRanking.get(player).getTotalSquaresCatched());
+        switch (option) {
+        case WINRATE:
+            playerList.sort(new WinRateComparator());
+            break;
+        case TOTAL_WINS:
 
-        return new Pair<>(player, datas);
+            break;
+        case TOTAL_MATCHES:
+
+            break;
+        case TOTAL_SQUARES_CATCHED:
+
+            break;
+        default:
+            throw new IllegalStateException("There is a bug here.");
+        }
+
+        return Collections.unmodifiableList(playerList);
     }
+    
 }
