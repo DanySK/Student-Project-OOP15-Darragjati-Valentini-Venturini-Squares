887c957cba94113856ef2550beb30fcb7a488ee9
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-11 11:32:20 +0200

    modificata la classe BaseGridImpl in modo che crei solamente il campo da gioco e fornisca getter e setter per inserire le mosse nella griglia

--- src/model/classes/BaseGridImpl.java
+++ src/model/classes/BaseGridImpl.java
@@ -17,10 +17,6 @@ public class BaseGridImpl implements BaseGrid {
     private List<List<GridOption>> horizontal = new ArrayList<>();
     private List<List<GridOption>> vertical = new ArrayList<>();
     private GridOption turn = GridOption.EMPTY;
-    private boolean matchStarted = false;
-    private Integer scorePlayer1;
-    private Integer scorePlayer2;
-    private static final Integer INITIAL_SCORE = 0;
     private static final Integer MINIMUM_SIZE = 4;
     private static final Integer MAXIMUM_SIZE = 10;
 
@@ -57,83 +53,6 @@ public class BaseGridImpl implements BaseGrid {
     }
 
     @Override
-    public void startMatch() {
-
-        if (!isStarted()) {
-            this.scorePlayer1 = INITIAL_SCORE;
-            this.scorePlayer2 = INITIAL_SCORE;
-            randomizeTurn();
-            matchStarted = true;
-        } else {
-            throw new IllegalStateException("Match già iniziato");
-        }
-    }
-
-    private void randomizeTurn() {
-
-        if (!isStarted()) {
-            Random randomTurn = new Random();
-
-            if (randomTurn.nextInt(2) == 0) {
-                this.turn = GridOption.PLAYER1;
-            } else {
-                this.turn = GridOption.PLAYER2;
-            }
-        } else {
-            throw new IllegalStateException();
-        }
-
-    }
-
-    @Override
-    public boolean isStarted() {
-        return this.matchStarted;
-    }
-
-    @Override
-    public boolean isEnded() {
-        return (!isStarted() || (scorePlayer1 + scorePlayer2) < horizontal.get(0).size() * horizontal.get(0).size())
-                ? false : true;
-    }
-
-    @Override
-    public GridOption getCurrentPlayerTurn() {
-        if (isStarted()) {
-            return this.turn;
-        } else {
-            throw new IllegalStateException();
-        }
-    }
-
-    private void nextTurn() {
-
-        if (!isStarted()) {
-            throw new IllegalStateException();
-        }
-
-        if (getCurrentPlayerTurn().equals(GridOption.PLAYER1)) {
-            this.turn = GridOption.PLAYER2;
-        } else {
-            this.turn = GridOption.PLAYER1;
-        }
-    }
-
-    @Override
-    public Integer getPlayerPoints(final GridOption player) {
-
-        if (!isStarted()) {
-            return INITIAL_SCORE;
-        }
-
-        if (player.equals(GridOption.PLAYER1) || player.equals(GridOption.PLAYER2)) {
-            return (player.equals(GridOption.PLAYER1)) ? scorePlayer1 : scorePlayer2;
-        } else {
-            throw new IllegalArgumentException();
-        }
-
-    }
-
-    @Override
     public Integer getTotalMoves() {
         return (horizontal.size() * horizontal.get(0).size()) + (vertical.size() * vertical.get(0).size());
     }
@@ -159,6 +78,16 @@ public class BaseGridImpl implements BaseGrid {
             }
         }
         return movesLeft;
+    }
+    
+    
+    public GridOption getCurrentPlayerTurn(){
+        
+        return this.turn;
+    }
+    
+    public void setPlayerTurn(final GridOption turn){
+        this.turn = turn;
     }
 
     private void checkCorrectVerticalInput(final Integer listIndex, final Integer position) {
@@ -187,38 +116,9 @@ public class BaseGridImpl implements BaseGrid {
 
         if (vertical.get(listIndex).get(position).equals(GridOption.EMPTY)) {
             vertical.get(listIndex).set(position, getCurrentPlayerTurn());
-
-            if (!verticalPointScored(listIndex, position)) {
-                nextTurn();
-            }
         } else {
             throw new IllegalStateException();
         }
-    }
-
-    private boolean verticalPointScored(final int listIndex, final int position) {
-
-        int points = 0;
-
-        if (listIndex != 0) {
-            if (getCopyOfVerticalElement(listIndex - 1, position) != GridOption.EMPTY) {
-                if (getCopyOfHorizontalElement(position, listIndex - 1) != GridOption.EMPTY
-                        && getCopyOfHorizontalElement(position + 1, listIndex - 1) != GridOption.EMPTY) {
-                    points++;
-                }
-            }
-        }
-
-        if (listIndex != vertical.get(0).size()) {
-            if (getCopyOfVerticalElement(listIndex + 1, position) != GridOption.EMPTY) {
-                if (getCopyOfHorizontalElement(position, listIndex) != GridOption.EMPTY
-                        && getCopyOfHorizontalElement(position + 1, listIndex) != GridOption.EMPTY) {
-                    points++;
-                }
-            }
-        }
-        addPoints(points);
-        return points > 0 ? true : false;
     }
 
     private void checkCorrectHorizontalInput(final Integer listIndex, final Integer position) {
@@ -247,54 +147,12 @@ public class BaseGridImpl implements BaseGrid {
 
         if (horizontal.get(listIndex).get(position).equals(GridOption.EMPTY)) {
             horizontal.get(listIndex).set(position, getCurrentPlayerTurn());
-
-            if (!horizontalPointScored(listIndex, position)) {
-                nextTurn();
-            }
-
         } else {
             throw new IllegalStateException();
         }
     }
 
-    private boolean horizontalPointScored(final int listIndex, final int position) {
-
-        int points = 0;
-
-        if (listIndex != 0) {
-            if (getCopyOfHorizontalElement(listIndex - 1, position) != GridOption.EMPTY) {
-                if (getCopyOfVerticalElement(position, listIndex - 1) != GridOption.EMPTY
-                        && getCopyOfVerticalElement(position + 1, listIndex - 1) != GridOption.EMPTY) {
-                    points++;
-                }
-            }
-        }
-
-        if (listIndex != horizontal.get(0).size()) {
-            if (getCopyOfHorizontalElement(listIndex + 1, position) != GridOption.EMPTY) {
-                if (getCopyOfVerticalElement(position, listIndex) != GridOption.EMPTY
-                        && getCopyOfVerticalElement(position + 1, listIndex) != GridOption.EMPTY) {
-                    points++;
-                }
-            }
-        }
-
-        addPoints(points);
-        return points > 0 ? true : false;
-    }
-
-    private void addPoints(final Integer points) {
-
-        if (!isStarted()) {
-            throw new IllegalStateException();
-        }
-
-        if (getCurrentPlayerTurn().equals(GridOption.PLAYER1)) {
-            scorePlayer1 += points;
-        } else {
-            scorePlayer2 += points;
-        }
-    }
+    
 
     /*
      * private GridOption getPreviousParallelList(final int listIndex, final int
@@ -313,18 +171,4 @@ public class BaseGridImpl implements BaseGrid {
      * return getCopyOfElement(listIndex + 1, position); } else { throw new
      * IllegalArgumentException(); } }
      */
-    @Override
-    public GridOption getWinner() {
-
-        if (isEnded()) {
-            if (getPlayerPoints(GridOption.PLAYER1).equals(getPlayerPoints(GridOption.PLAYER2))) {
-                return GridOption.EMPTY;
-            }
-
-            return (getPlayerPoints(GridOption.PLAYER1) > getPlayerPoints(GridOption.PLAYER2)) ? GridOption.PLAYER1
-                    : GridOption.PLAYER2;
-        } else {
-            throw new IllegalStateException();
-        }
-    }
 }
---
+++ src/model/interfaces/Turn.java
@@ -0,0 +1,75 @@
+package model.interfaces;
+
+import model.enumerations.GridOption;
+
+public interface Turn {
+
+    /**
+     * Starts a new match.
+     * @throws a IllegalStateException if the match is already started
+     */
+    void startMatch();
+
+    /**
+     * 
+     * @return true if the game is started
+     */
+    boolean isStarted();
+
+    /**
+     * 
+     * @return if there are no more moves left
+     */
+    boolean isEnded();
+
+    /**
+     * 
+     * @return the player who should take the next move
+     * @throws a IllegalStateException if the match is not started
+     */
+    GridOption getCurrentPlayerTurn();
+
+    /**
+     * Gets the score of one of the two players.
+     * @param player is one of the players
+     * @return his points
+     * @throws a IllegalArgumentException if player does not exist
+     */
+    Integer getPlayerPoints(GridOption player);
+
+    /**
+     * 
+     * @return the number of moves
+     */
+    Integer getTotalMoves();
+
+    /**
+     * 
+     * @return the number of moves left
+     */
+    Integer getRemainingMoves();
+
+    /**
+     * 
+     * @param listIndex is the number of list
+     * @param elementIndex is the position of the chosen list where you want to get the line
+     * @return which player has set the vertical line, if it is empty noone has set that line
+     * @throws a IllegalArgumentException if the parameters insert are not correct
+     */
+    GridOption getCopyOfElement(Integer listIndex, Integer elementIndex);
+
+    /**
+     * Makes a move setting a line in the grid.
+     * @param listIndex is the number of the list where the player wants to set his line
+     * @param position is the position of the chosen list where the player wants to set the line
+     * @throws a IllegalArgumentException if the parameters insert are not correct
+     */
+    void setLine(int listIndex, int position);
+
+    /**
+     * 
+     * @return the winner of the match, if the game is even returns GridOption.Empty
+     * @throws a IllegalStateException if the game is not ended
+     */
+    GridOption getWinner();
+}
