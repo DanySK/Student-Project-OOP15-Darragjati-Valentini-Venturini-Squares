bd7e645e65f84920581f49334c884f2ffad86262
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-20 17:17:32 +0200

    aggiunta la descrizione dell'interfaccia LastMove. Aggiunta descrizione anche ai getter e setter

--- src/model/interfaces/LastMove.java
+++ src/model/interfaces/LastMove.java
@@ -3,46 +3,48 @@ package model.interfaces;
 import model.enumerations.ListType;
 
 /**
- * 
- * 
- *
+ * This interface is used to organize the last move done by one of the two
+ * players through getters and setters.
  */
 public interface LastMove {
 
     /**
-     * 
+     * Gets wich one of the two list types was used last.
      * @return the last list used, it is respectively ListType.HORIZONTAL or
      *         ListType.VERTICAL if it is the horizontal or the vertical one
      */
     ListType getLastListType();
 
     /**
-     * 
-     * @param lastListType the type of the last list used
+     * Sets wich one of the two list types was used last.
+     * @param lastListType
+     *            the type of the last list used
      */
     void setLastListType(ListType lastListType);
 
     /**
-     * 
+     * Gets wich one of the "n" lists was used last.
      * @return the number of the "n" list was used last
      */
     Integer getLastListIndex();
 
     /**
-     * 
-     * @param lastListIndex which one of the "n" lists is used last
+     * Sets wich one of the "n" lists was used last.
+     * @param lastListIndex
+     *            which one of the "n" lists is used last
      */
     void setLastListIndex(Integer lastListIndex);
 
     /**
-     * 
-     * @return the last index used to insert a move in the list
+     * Gets the last index used to make a move.
+     * @return the last index used to make a move
      */
     Integer getLastPosition();
 
     /**
-     * 
-     * @param lastPosition the index of the list where was insert the last move
+     * Sets the last index used to make a move.
+     * @param lastPosition
+     *            the index of the list where was insert the last move
      */
     void setLastPosition(Integer lastPosition);
 
