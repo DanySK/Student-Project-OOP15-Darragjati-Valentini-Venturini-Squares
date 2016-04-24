49af1bd5051d88e4b9e2bd1c71f807b3d6b11652
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-24 16:17:09 +0200

    aggiunta la descrizione dell'enum ListType. Aggiunto anche il nuovo campo DIAGONAL

--- src/model/enumerations/ListType.java
+++ src/model/enumerations/ListType.java
@@ -1,11 +1,9 @@
 package model.enumerations;
 
 /**
- * 
- * 
- *
+ * This enum represents wich one of the lists is chosen to make a move.
  */
 public enum ListType {
 
-    HORIZONTAL, VERTICAL;
+    HORIZONTAL, VERTICAL, DIAGONAL;
 }
