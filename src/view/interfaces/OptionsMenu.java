b687e17f0e8d81086369d19a18b185491d2e187f
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-24 15:23:38 +0200

    aggiunta la descrizione dell'interfaccia e dei metodi di OptionsMenu

--- src/view/interfaces/OptionsMenu.java
+++ src/view/interfaces/OptionsMenu.java
@@ -1,5 +1,34 @@
 package view.interfaces;
 
+/**
+ * This interface is used to custom the application's interface. The user can
+ * lower or increase the application's volume and choose the colours of the
+ * background and the players.
+ */
 public interface OptionsMenu {
 
+    /**
+     * sets the application's volume higher.
+     */
+    void setVolumeHigher();
+
+    /**
+     * sets the application's volume lower.
+     */
+    void setVolumeLower();
+
+    /**
+     * sets the first player's colour.
+     */
+    void setFirstPlayerColour();
+
+    /**
+     * sets the second player's colour.
+     */
+    void setSecondPlayerColour();
+
+    /**
+     * sets the application's background.
+     */
+    void setBackground();
 }
