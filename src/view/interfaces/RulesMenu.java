463ee5262d4ed12acb0b215fa5381da1772deb2d
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-24 16:05:33 +0200

    aggiunta la descrizione dell'interfaccia RulesMenu

--- src/view/interfaces/RulesMenu.java
+++ src/view/interfaces/RulesMenu.java
@@ -1,5 +1,13 @@
 package view.interfaces;
 
+/**
+ * This interface is used to show the game's rules.
+ *
+ */
 public interface RulesMenu {
 
+    /**
+     * show the rules of the game.
+     */
+    void showRules();
 }
