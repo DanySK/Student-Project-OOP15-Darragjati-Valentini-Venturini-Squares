2364fa744ab071bec629ea39650ef9dc29c1bcdb
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-24 16:21:30 +0200

    aggiunte le descrizioni delle enum RankingOption e GridOption

--- src/model/enumerations/GridOption.java
+++ src/model/enumerations/GridOption.java
@@ -3,14 +3,9 @@ package model.enumerations;
 
 
 /**
- * 
- * 
- *
+ * This enum represents the state of a line in the grid can assume.
  */
 public enum GridOption {
 
-    /**
-     * 
-     */
     EMPTY, PLAYER1, PLAYER2;
 }
--- src/model/enumerations/RankingOption.java
+++ src/model/enumerations/RankingOption.java
@@ -2,14 +2,9 @@ package model.enumerations;
 
 
 /**
- * 
- * 
- *
+ * This enum represents the possibilities to order the ranking.
  */
 public enum RankingOption {
 
-    /**
-     * 
-     */
     WINRATE, TOTAL_WINS, TOTAL_MATCHES, TOTAL_SQUARES_CATCHED;
 }
