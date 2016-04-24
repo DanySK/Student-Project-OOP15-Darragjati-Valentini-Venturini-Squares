5840437b1a8dd2a5117f3ec8fb3b0187f82e7d56
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-24 15:06:05 +0200

    aggiunti i metodi principali e la loro descrizione all'interfaccia StartMenu

--- src/view/interfaces/StartMenu.java
+++ src/view/interfaces/StartMenu.java
@@ -1,5 +1,39 @@
 package view.interfaces;
 
+/**
+ * This interface is used to choose the main options of the appication: to start
+ * a new game, to show the rules, to show the ranking, to change the main
+ * settings and to quit the application.
+ */
 public interface StartMenu {
 
+    /**
+     * this method shows or not the starting menu.
+     */
+    void showStartMenu();
+
+    /**
+     * this method starts a new match.
+     */
+    void startGame();
+
+    /**
+     * this method shows the rules of the game.
+     */
+    void showRules();
+
+    /**
+     * this method shows the ranking of the players.
+     */
+    void showRanking();
+
+    /**
+     * this method shows the options.
+     */
+    void showSettings();
+
+    /**
+     * this method quits the application.
+     */
+    void quit();
 }
