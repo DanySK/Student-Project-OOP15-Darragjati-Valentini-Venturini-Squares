c79a8c08781ccd9641179516dc79586b2a60ae64
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-04-24 15:29:54 +0200

    aggiunta la descrizione dell'interfaccia RankingMenu e dei suoi metodi principali

--- src/view/interfaces/RankingMenu.java
+++ src/view/interfaces/RankingMenu.java
@@ -1,5 +1,30 @@
 package view.interfaces;
 
+/**
+ * This interface is used to manage the ranking. The ordering options are: by
+ * winrate, by total wins, by total matches and total conquered points.
+ * 
+ *
+ */
 public interface RankingMenu {
 
+    /**
+     * orders the ranking by win rate.
+     */
+    void orderByWinrate();
+
+    /**
+     * orders the ranking by total wins.
+     */
+    void orderByTotalWins();
+
+    /**
+     * orders the ranking by total matches.
+     */
+    void orderByTotalMatches();
+
+    /**
+     * orders the ranking by total conquered points.
+     */
+    void orderByTotalSquaresCatched();
 }
