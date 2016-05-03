9cacc8e9135d7c1bcd39fd1f89393d20699f8131
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-03 11:30:30 +0200

    Aggiunti quelli che dovrebbero essere i principali metodi della classe, ossia i metodi da chiamare per ciascuno del menu d'avvio(start game, ranking, setting, rules, exit). Implementazione da completare.

--- src/controller/classes/Menu.java
+++ src/controller/classes/Menu.java
@@ -1,15 +1,43 @@
 package controller.classes;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Menu {
-    
-    private final String directory;
-    
-    public Menu(String directory) {
-        this.directory = directory;
-    }
-    
-    public void startGame() {
-        StartGame game = new StartGame();
+
+    private String lineRead;
+
+    public String getLineRead() {
+        return this.lineRead;
     }
 
+    // public Menu(String directory) {
+    // this.directory = directory;
+    // }
+
+    public void startGame(int numColonne, int numRighe) {
+        StartGame game = new StartGame(numColonne, numRighe);
+        
+    }
+
+    public String showRules() {
+
+        FileReader f = new FileReader("");
+
+        BufferedReader b = new BufferedReader(f);
+
+        return getLineRead();
+
+    }
+    
+    public String showRanking(){
+        return ;
+    }
+    
+    public void setting(){
+        
+    }
+    
+    public void exit(){
+    }
 }
