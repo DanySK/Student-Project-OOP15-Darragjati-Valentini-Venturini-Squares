e16793b1c08f1c1b7aa1c467237389881990e693
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-04 11:41:28 +0200

    Modificato metodo per l'aggiunta del nome dei giocatori, ora non passa pi? i nomi al model, ma fa semplicemente il controllo sui nomi (da public a private).

--- src/controller/classes/StartGame.java
+++ src/controller/classes/StartGame.java
@@ -12,35 +12,37 @@ public class StartGame {
     private String namePlayer2;
 
     public StartGame(int numColonne, int numRighe, String namePlayer1, String namePlayer2) {
+        controlNamePlayers(namePlayer1, namePlayer2);
         this.numColonne = numColonne;
         this.numRighe = numRighe;
         this.namePlayer1 = namePlayer1;
         this.namePlayer2 = namePlayer2;
     }
 
-    public void createGrid(int numColonne, int numRighe){
+    public void createGrid(int numColonne, int numRighe) {
         BaseGridImpl newGrid = new BaseGridImpl(numRighe, numColonne);
-    
+
     }
-    
-    public void addPlayer(String namePlayer1, String namePlayer2) {
-        PlayerImpl player1 = new PlayerImpl(namePlayer1);
-        PlayerImpl player2 = new PlayerImpl(namePlayer2);        
-        
+
+    private void controlNamePlayers(String namePlayer1, String namePlayer2) {
+        if (namePlayer1.equals(namePlayer2)) {
+            throw new IllegalArgumentException();
+        }
+
     }
-    
-    public int getNumColonne(){
+
+    public int getNumColonne() {
         return this.numColonne;
     }
-    
-    public int getNumRighe(){
+
+    public int getNumRighe() {
         return this.numRighe;
     }
-    
+
     public String getNamePlayer1() {
         return this.namePlayer1;
     }
-    
+
     public String getNamePlayer2() {
         return this.namePlayer2;
     }
