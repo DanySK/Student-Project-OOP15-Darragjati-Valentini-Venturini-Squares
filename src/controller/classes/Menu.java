e705b40be8e31e9f1d51b72b7ff15b9cc81579d4
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-04 11:40:12 +0200

    Implementato metodo di lettura file regole, bisogna risolvere il problema della directory. Aggiunto metodo chiusura applicazione.

--- src/controller/classes/Menu.java
+++ src/controller/classes/Menu.java
@@ -1,43 +1,57 @@
 package controller.classes;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Menu {
 
-    private String lineRead;
+    private String linesRead;
 
-    public String getLineRead() {
-        return this.lineRead;
-    }
+    // public String getLineRead() {
+    // return this.lineRead;
+    // }
 
     // public Menu(String directory) {
     // this.directory = directory;
     // }
 
-    public void startGame(int numColonne, int numRighe) {
-        StartGame game = new StartGame(numColonne, numRighe);
-        
-    }
-
-    public String showRules() {
-
-        FileReader f = new FileReader("");
-
-        BufferedReader b = new BufferedReader(f);
-
-        return getLineRead();
+    public void startGame(final int numColonne, final int numRighe, final String namePlayer1,
+            final String namePlayer2) {
+        StartGame game = new StartGame(numColonne, numRighe, namePlayer1, namePlayer2);
 
     }
-    
-    public String showRanking(){
-        return ;
+
+    public String showRules() throws IOException {
+
+        String fileName = System.getProperty("user.home") + System.getProperty("file.separator") System.getProperty("")+ "SquareRules.txt";
+        File file = new File(fileName); // fileName è di tipo String ed è il
+                                        // percorso del file
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String s;
+            while ((s = br.readLine()) != null) {
+                 this.linesRead.concat(s);
+                 //System.out.println(s);
+            }
+        } catch (IOException ex) {
+            throw new IOException();
+        }
+        System.out.println(this.linesRead);
+        return this.linesRead;
     }
-    
-    public void setting(){
-        
+
+    public String showRanking() {
+        return;
     }
-    
-    public void exit(){
+
+    public void setting() {
+
+    }
+
+    public void exit() {
+        System.exit(0);
     }
 }
