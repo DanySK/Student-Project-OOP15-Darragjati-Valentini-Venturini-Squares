324e58e96d100f073dddcf9e5e160524c15321af
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-06 18:16:31 +0200

    Fatte alcune modifiche, ma poi cancellate perch? non funzionanti.

--- src/controller/classes/AddMove.java
+++ src/controller/classes/AddMove.java
@@ -2,20 +2,28 @@ package controller.classes;
 
 import model.enumerations.GridOption;
 import model.enumerations.ListType;
+import model.interfaces.Turn;
+
 
 public class AddMove {
     private ListType direction;
     private int position;
     private int numLine;
     
-    public AddMove(ListType direction, int numLine, int position){
+    
+    public AddMove(final ListType direction, final int numLine, final int position){
         this.direction = direction;
         this.numLine = numLine;
         this.position = position;
+        
+       
                 
     }
     
     public void addLine(){
+       
         
+        
+                
     }
 }
