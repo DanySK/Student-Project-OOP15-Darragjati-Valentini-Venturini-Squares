2a8e409499b9a05c6b9dea6d00025d54122db04b
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-06 16:36:18 +0200

    Aggiunto costruttore alla classe AddMove.

--- src/controller/classes/AddMove.java
+++ src/controller/classes/AddMove.java
@@ -1,5 +1,21 @@
 package controller.classes;
 
-public class AddMove {
+import model.enumerations.GridOption;
+import model.enumerations.ListType;
 
+public class AddMove {
+    private ListType direction;
+    private int position;
+    private int numLine;
+    
+    public AddMove(ListType direction, int numLine, int position){
+        this.direction = direction;
+        this.numLine = numLine;
+        this.position = position;
+                
+    }
+    
+    public void addLine(){
+        
+    }
 }
