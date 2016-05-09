f9c6358a2b7eaed36558821cf0ee53a6fddca78c
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-09 11:38:42 +0200

    Refactor del metodo randomPlayer in namePlayer

--- src/controller/classes/AddMove.java
+++ src/controller/classes/AddMove.java
@@ -7,9 +7,9 @@ import model.interfaces.BaseGrid;
 import model.interfaces.Turn;
 
 public class AddMove {
-    private ListType direction;
-    private int position;
-    private int numLine;
+    // private ListType direction;
+    // private int position;
+    // private int numLine;
     private Turn turn;
     private GridOption firstMove;
 
@@ -17,17 +17,10 @@ public class AddMove {
         this.turn = new GameImpl(grid);
     }
 
-    public AddMove(final ListType direction, final int numLine, final int position) {
-        this.direction = direction;
-        this.numLine = numLine;
-        this.position = position;
-
+    public void addLine(ListType direction, int numLine, int position) {
+        turn.setLine(direction, numLine, position);
     }
 
-    public void addLine() {
-        turn.setLine(this.direction, this.numLine, this.position);
-    }
-    
     public GridOption firstPlayer() {
         turn.startMatch();
         this.firstMove = turn.getCurrentPlayerTurn();
