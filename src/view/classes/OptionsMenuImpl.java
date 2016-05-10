765d010dd8e4dfbe7bf78b62bff17ef9f00de696
Karl Darragjati <karl.darragjati@studio.unibo.it>
2016-05-10 14:24:00 +0200

    Aggiunta classe OptionsMenuImpl

---
+++ src/view/classes/OptionsMenuImpl.java
@@ -0,0 +1,37 @@
+package view.classes;
+
+import view.interfaces.*;
+
+public class OptionsMenuImpl implements OptionsMenu {
+
+	@Override
+	public void setVolumeHigher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setVolumeLower() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFirstPlayerColour() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSecondPlayerColour() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBackground() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
