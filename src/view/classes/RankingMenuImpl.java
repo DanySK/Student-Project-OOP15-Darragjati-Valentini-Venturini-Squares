cb8776a83f4654b92a6c6f21c6c6cc90c5ce1795
Karl Darragjati <karl.darragjati@studio.unibo.it>
2016-05-10 14:25:20 +0200

    Aggiunta classe RankingMenuImpl

---
+++ src/view/classes/RankingMenuImpl.java
@@ -0,0 +1,31 @@
+package view.classes;
+
+import view.interfaces.*;
+
+public class RankingMenuImpl implements RankingMenu {
+
+	@Override
+	public void orderByWinrate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void orderByTotalWins() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void orderByTotalMatches() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void orderByTotalSquaresCatched() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
