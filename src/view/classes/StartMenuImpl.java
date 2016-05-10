00820af6866653b814e75f3f2d7db85abcd3e7fd
Karl Darragjati <karl.darragjati@studio.unibo.it>
2016-05-10 14:15:04 +0200

    Aggiunta classe StartMenuImpl

---
+++ src/view/classes/StartMenuImpl.java
@@ -0,0 +1,43 @@
+package view.classes;
+
+import view.interfaces.*;
+
+public class StartMenuImpl implements StartMenu {
+
+	@Override
+	public void showStartMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startNewGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showRules() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showRanking() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
