a792b40280372e822f129305525d7c0f1165ddf3
Karl Darragjati <karl.darragjati@studio.unibo.it>
2016-05-10 14:19:52 +0200

    Aggiunta classe MatchSetupImpl

---
+++ src/view/classes/MatchSetupImpl.java
@@ -0,0 +1,25 @@
+package view.classes;
+
+import view.interfaces.*;
+
+public class MatchSetupImpl implements MatchSetup{
+
+	@Override
+	public void startMatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void borderless() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBoardType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
