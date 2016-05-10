bd81cfd4beb063dc02db2540c1d282a89bb4847a
Karl Darragjati <karl.darragjati@studio.unibo.it>
2016-05-10 14:26:51 +0200

    Aggiunta classe RulesMenuImpl

---
+++ src/view/classes/RulesMenuImpl.java
@@ -0,0 +1,13 @@
+package view.classes;
+
+import view.interfaces.*;
+
+public class RulesMenuImpl implements RulesMenu {
+
+	@Override
+	public void showRules() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
