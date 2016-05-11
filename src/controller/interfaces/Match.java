97a3d63aaa51b2c7e1e15dbf6b94812633fc2bde
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-11 10:39:05 +0200

    Aggiunta interfaccia Match, completa dei metodi che sono stati implementati per ora.

---
+++ src/controller/interfaces/Match.java
@@ -0,0 +1,32 @@
+package controller.interfaces;
+
+import model.enumerations.ListType;
+
+public interface Match {
+
+    /**
+     * Metodo che crea la griglia di gioco, a seconda della tipologia di gioco scelta.
+     */
+    void createGrid();
+    
+    /**
+     * Metodo che genera una nuova partita e fa partire il tempo di gioco.
+     * @return Nome del giocatore che deve fare la prima mossa
+     */
+    String createNewMatch();
+    
+    /**
+     * Metodo che permette di aggiungere una mossa e controlla se la partita è finita.
+     * @param direction Verticale, orizzontale o diagonale (se la griglia è triangolare).
+     * @param numLine Numero linea in cui è posta la mossa.
+     * @param position Posizione nella linea della mossa.
+     * @return Nome del giocatore che deve fare la prossima mossa
+     */
+    String addLine(ListType direction, int numLine, int position);
+    
+    /**
+     * Metodo che fa l'undo dell'ultima mossa.
+     */
+    void undo();
+    
+}
