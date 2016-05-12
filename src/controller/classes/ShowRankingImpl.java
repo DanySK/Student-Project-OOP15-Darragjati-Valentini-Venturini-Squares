7984883fb4871c0c616228d92358e022bc08a034
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-12 12:40:52 +0200

    Aggiunta classe ShowRankingImpl

---
+++ src/controller/classes/ShowRankingImpl.java
@@ -0,0 +1,19 @@
+package controller.classes;
+
+import controller.interfaces.ShowRanking;
+import model.enumerations.RankingOption;
+
+public class ShowRankingImpl implements ShowRanking {
+
+    @Override
+    public String showRanking(RankingOption rankingOrder, boolean reverse) {
+        
+        return null;
+    }
+    
+    private void createRanking(){
+        
+    }
+    
+
+}
