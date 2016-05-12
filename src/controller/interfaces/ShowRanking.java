88fd098d685f09a8ed0b07aa5ae02009b8843f1e
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-12 11:06:48 +0200

    Aggiunta interfaccia ShowRanking, ancora da aggiungere la rispettiva classe.

---
+++ src/controller/interfaces/ShowRanking.java
@@ -0,0 +1,11 @@
+package controller.interfaces;
+
+import model.enumerations.RankingOption;
+
+public interface ShowRanking {
+    
+    String showRanking(RankingOption rankingOrder, boolean reverse);
+    
+    
+
+}
