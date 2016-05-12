464371be810eff97a366b80f05bc9ee34c1ffba4
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-12 11:05:34 +0200

    Aggiunta classe SettingImpl.

---
+++ src/controller/classes/SettingImpl.java
@@ -0,0 +1,31 @@
+package controller.classes;
+
+import controller.interfaces.Setting;
+
+public class SettingImpl implements Setting {
+
+    @Override
+    public void setColorPlayer1() {
+        
+
+    }
+
+    @Override
+    public void setColorPlayer2() {
+        
+
+    }
+
+    @Override
+    public void turnVolumeOn() {
+        
+
+    }
+
+    @Override
+    public void turnVolumeOff() {
+        
+
+    }
+
+}
