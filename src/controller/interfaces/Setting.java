968101665e418d485e0e6ef655a1fd349f64bf11
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-12 11:06:09 +0200

    Aggiunta interfaccia Setting

---
+++ src/controller/interfaces/Setting.java
@@ -0,0 +1,13 @@
+package controller.interfaces;
+
+public interface Setting {
+    
+    void setColorPlayer1();
+    
+    void setColorPlayer2();
+    
+    void turnVolumeOn();
+    
+    void turnVolumeOff();
+
+}
