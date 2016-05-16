db6fbba8f7b0b8e157ce58275adc2f6c613a90c8
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-16 16:42:26 +0200

    Sistemata javadoc dell'interfaccia showRanking

--- src/controller/interfaces/ShowRanking.java
+++ src/controller/interfaces/ShowRanking.java
@@ -1,11 +1,22 @@
 package controller.interfaces;
 
 import model.enumerations.RankingOption;
-
+/**
+ * 
+ * @author Licia Valentini
+ * 
+ * Interfaccia della classe dedicata alla gestione della classifica.
+ *
+ */
 public interface ShowRanking {
-    
+     /**
+     * Metodo per la visualizzazione della lista, i parametri servono per modificare il criterio d'ordine.
+     * @param rankingOrder
+     * @param reverse
+     * @return classifica sotto forma di stringa.
+     */
     String showRanking(RankingOption rankingOrder, boolean reverse);
-    
+   
     
 
 }
