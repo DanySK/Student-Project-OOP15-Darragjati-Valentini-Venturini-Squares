c30d42c2c2df5f69e6fdd997cef108be12eb9411
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-16 19:02:22 +0200

    Aggiunto file con regole.

--- src/controller/classes/MenuImpl.java
+++ src/controller/classes/MenuImpl.java
@@ -1,6 +1,8 @@
 package controller.classes;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,35 +11,35 @@ import controller.interfaces.Menu;
 import controller.interfaces.ShowRanking;
 
 public class MenuImpl implements Menu {
-    
-    private String rules;
-    
-    public MenuImpl() {
-        
-    }
-    
-    public String rules() throws IOException{
-        
-        
-        InputStream readFile = ClassLoader.class.getResourceAsStream("res/Rules.txt");
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(readFile, "UTF8"))) {
+    private String rules;
+
+    public MenuImpl() {
+
+    }
+
+    public String rules() throws IOException {
+
+        String txtDirectory = ClassLoader.class.getResource("/Rules.txt").getPath();
+        InputStream in = ClassLoader.class.getResourceAsStream(txtDirectory);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF8"))) {
             String s;
             while ((s = br.readLine()) != null) {
+                System.out.println(s);
                 this.rules = this.rules.concat(s);
 
             }
         } catch (IOException ex) {
             throw new IOException();
         }
-        readFile.close(); 
+        in.close();
         return this.rules;
-        
+
     }
-    
-    public void exitApp(){
+
+    public void exitApp() {
         System.exit(0);
-    }   
-    
+    }
 
 }
