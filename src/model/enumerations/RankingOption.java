2f2010a52a27d1fd7333dd85b84d794fe34698c4
GIacomo Venturini <giacomo.venturini2@studio.unibo.it>
2016-05-17 21:57:35 +0200

    sistemata la javadoc dell'enumerazione RankingOptions

--- src/model/enumerations/RankingOption.java
+++ src/model/enumerations/RankingOption.java
@@ -1,15 +1,16 @@
 package model.enumerations;
 
 /**
- * This enum represents the possibilities to order the ranking.
+ * This enum is used to order the ranking. The players can choose between four
+ * ordering options.
  */
 public enum RankingOption {
 
     /**
-     * WINRATE is used to order the ranking by the winrate.
-     * TOTAL_WINS is used to order the ranking by the number of wins.
-     * TOTAL_MATCHES is used to order the ranking by the number of games played.
-     * TOTAL_SQUARES_CATCHED is used to order the ranking by the total points scored.
+     * WINRATE is used to order the ranking by the winrate. TOTAL_WINS is used
+     * to order the ranking by the number of wins. TOTAL_MATCHES is used to
+     * order the ranking by the number of games played. TOTAL_POINTS_CATCHED is
+     * used to order the ranking by the total points scored.
      */
     WINRATE, TOTAL_WINS, TOTAL_MATCHES, TOTAL_POINTS_SCORED;
 }
