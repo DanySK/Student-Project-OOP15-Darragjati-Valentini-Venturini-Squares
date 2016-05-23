b68184dec0c992e3528c68a7ded905f0aed8cb3f
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-23 19:38:52 +0200

    Adattata interfaccia ShowRanking alla relativa classe.

--- src/it/unibo/squaresgameteam/squares/controller/interfaces/ShowRanking.java
+++ src/it/unibo/squaresgameteam/squares/controller/interfaces/ShowRanking.java
@@ -1,6 +1,10 @@
 package it.unibo.squaresgameteam.squares.controller.interfaces;
 
+import java.io.IOException;
+
 import it.unibo.squaresgameteam.squares.model.enumerations.RankingOption;
+import it.unibo.squaresgameteam.squares.model.exceptions.DuplicatedPlayerStatsException;
+import it.unibo.squaresgameteam.squares.model.interfaces.Player;
 /**
  * 
  * @author Licia Valentini
@@ -14,8 +18,12 @@ public interface ShowRanking {
      * @param rankingOrder
      * @param reverse
      * @return classifica sotto forma di stringa.
+     * @throws IOException 
+     * @throws DuplicatedPlayerStatsException 
      */
-    String showRanking(RankingOption rankingOrder, boolean reverse);
+    String showRanking(RankingOption rankingOrder, boolean reverse) throws IOException, DuplicatedPlayerStatsException;
+    
+    void addPlayer(String player) throws IOException, DuplicatedPlayerStatsException;
    
     
 
