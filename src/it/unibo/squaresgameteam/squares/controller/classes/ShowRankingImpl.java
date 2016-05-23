0e8593d29081c70244195a5eeb1f4190097e6c1d
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-23 19:37:32 +0200

    Aggiunti vari metodi per completare la classe. Presenta errori causa modifiche del model, che verranno corretti poi.

--- src/it/unibo/squaresgameteam/squares/controller/classes/ShowRankingImpl.java
+++ src/it/unibo/squaresgameteam/squares/controller/classes/ShowRankingImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.squaresgameteam.squares.controller.classes;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,29 +16,44 @@ import it.unibo.squaresgameteam.squares.model.classes.RankingImpl;
 import it.unibo.squaresgameteam.squares.model.enumerations.RankingOption;
 import it.unibo.squaresgameteam.squares.model.exceptions.DuplicatedPlayerStatsException;
 import it.unibo.squaresgameteam.squares.model.interfaces.Player;
+import it.unibo.squaresgameteam.squares.model.interfaces.Ranking;
 
 public class ShowRankingImpl implements ShowRanking {
 
-    private String ranking;
+    private String rankingString;
+    private File rankingFile;
+    private Ranking rankingList;
 
     @Override
-    public String showRanking(RankingOption rankingOrder, boolean reverse) {
+    public String showRanking(RankingOption rankingOrder, boolean reverse)
+            throws IOException, DuplicatedPlayerStatsException {
+        createRanking();
+        this.rankingList.orderListBy(rankingOrder, reverse);
+        readRanking();
+        return this.rankingString;
+    }
 
-        return null;
+    @Override
+    public void addPlayer(String playerName, boolean win, Integer score)
+            throws IOException, DuplicatedPlayerStatsException {
+        createRanking();
+        rankingList.addPlayerResults(playerName, win, score);
+
     }
 
     private void createRanking() throws IOException, DuplicatedPlayerStatsException {
-
+        checkRankingFile();
         List<Player> currentRanking = new ArrayList<>();
-        InputStream readFile = ShowRankingImpl.class.getResourceAsStream("Ranking.txt");
+        InputStream readFile = ShowRankingImpl.class.getResourceAsStream(
+                System.getProperty("user.home") + System.getProperty("file.separator") + "Ranking.txt");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(readFile, "UTF8"))) {
-            String s = null;
+            String s;
             String playerName;
             String strWonMatches;
             String srtTotalMatches;
             String strTotalPointsScored;
-            StringTokenizer st = new StringTokenizer(s, "  ");
             while ((s = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(s, "   ");
                 playerName = st.nextToken();
                 strWonMatches = st.nextToken();
                 int wonMatches = Integer.parseInt(strWonMatches);
@@ -45,7 +61,8 @@ public class ShowRankingImpl implements ShowRanking {
                 int totalMatches = Integer.parseInt(srtTotalMatches);
                 strTotalPointsScored = st.nextToken();
                 int totalPointsScored = Integer.parseInt(strTotalPointsScored);
-                Player player = new PlayerImpl.Builder().playerName(playerName).wonMatches(wonMatches).totalMatches(totalMatches).totalPointsScored(totalPointsScored).build();
+                Player player = new PlayerImpl.Builder().playerName(playerName).wonMatches(wonMatches)
+                        .totalMatches(totalMatches).totalPointsScored(totalPointsScored).build();
                 currentRanking.add(player);
 
             }
@@ -53,17 +70,19 @@ public class ShowRankingImpl implements ShowRanking {
             throw new IOException();
         }
 
-        RankingImpl ranking = new RankingImpl(currentRanking);
+        this.rankingList = new RankingImpl(currentRanking);
 
     }
 
     private void readRanking() throws IOException {
-        InputStream readFile = ShowRanking.class.getResourceAsStream("Ranking.txt");
+        checkRankingFile();
+        InputStream readFile = ShowRanking.class.getResourceAsStream(
+                System.getProperty("user.home") + System.getProperty("file.separator") + "Ranking.txt");
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(readFile, "UTF8"))) {
             String s;
             while ((s = br.readLine()) != null) {
-                this.ranking = this.ranking + s;
+                this.rankingString = this.rankingString + s;
 
             }
         } catch (IOException ex) {
@@ -72,7 +91,17 @@ public class ShowRankingImpl implements ShowRanking {
         readFile.close();
 
     }
-    
-    
 
+    private void checkRankingFile() throws IOException {
+
+        this.rankingFile = new File(
+                System.getProperty("user.home") + System.getProperty("file.separator") + "Ranking.txt");
+        if (!this.rankingFile.exists()) {
+            this.rankingFile.createNewFile();
+        }
+    }
+
+    private void writeRankingFile() {
+
+    }
 }
