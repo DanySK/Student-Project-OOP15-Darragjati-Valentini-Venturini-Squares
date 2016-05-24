fd98ff1e9ad0535e3413ff0bd4437e83e0bd40f3
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-24 19:12:22 +0200

    Modificato main per testare la classe ShowRankingImpl.

--- src/it/unibo/squaresgameteam/squares/application/Application.java
+++ src/it/unibo/squaresgameteam/squares/application/Application.java
@@ -3,17 +3,19 @@ package it.unibo.squaresgameteam.squares.application;
 import java.io.IOException;
 
 import it.unibo.squaresgameteam.squares.controller.classes.MenuImpl;
+import it.unibo.squaresgameteam.squares.controller.classes.ShowRankingImpl;
 import it.unibo.squaresgameteam.squares.controller.interfaces.Menu;
+import it.unibo.squaresgameteam.squares.controller.interfaces.ShowRanking;
+import it.unibo.squaresgameteam.squares.model.enumerations.RankingOption;
+import it.unibo.squaresgameteam.squares.model.exceptions.DuplicatedPlayerStatsException;
 
 public class Application {  
 
-    public static void main(String[] args) throws IOException{
-        
-        Menu newMenu = new MenuImpl();
-     
-        String print = newMenu.rules();
-        System.out.println(print);
-       
+    public static void main(String[] args) throws IOException, DuplicatedPlayerStatsException{
+        String s;
+        ShowRanking ranking = new ShowRankingImpl();
+        s = ranking.showRanking(RankingOption.TOTAL_MATCHES, false);
+       System.out.println(s);
         
         
     }
