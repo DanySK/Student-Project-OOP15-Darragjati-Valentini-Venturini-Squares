74fc5c3d946275584bda28f067e8c2beb914a6cd
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-25 23:16:29 +0200

    Aggiunti metodi per l'accensione e lo spegnimento dell'audio di gioco. Funzionante.

--- src/it/unibo/squaresgameteam/squares/controller/classes/SettingImpl.java
+++ src/it/unibo/squaresgameteam/squares/controller/classes/SettingImpl.java
@@ -1,30 +1,64 @@
 package it.unibo.squaresgameteam.squares.controller.classes;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import it.unibo.squaresgameteam.squares.controller.interfaces.Setting;
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
 
 public class SettingImpl implements Setting {
 
-    @Override
-    public void setColorPlayer1() {
-        
+    private AudioPlayer player;
+    private ContinuousAudioDataStream loop;
 
-    }
-
-    @Override
-    public void setColorPlayer2() {
-        
+    public SettingImpl() {
 
     }
 
     @Override
     public void turnVolumeOn() {
-        
+
+        this.player.start(loop);
 
     }
 
     @Override
     public void turnVolumeOff() {
-        
+
+        this.player.stop(loop);
+
+    }
+
+    public void createAudio() throws IOException {
+
+        final File audio = new File(ClassLoader.class.getResource("/pol.au").getPath());
+
+        final InputStream in = new FileInputStream(audio);
+
+        try (final AudioStream stream = new AudioStream(in);) {
+            this.player = AudioPlayer.player;
+            final AudioData data = stream.getData();
+            this.loop = new ContinuousAudioDataStream(data);
+            this.player.start(loop);
+        }
+
+    }
+
+    @Override
+    public void setColorPlayer1() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setColorPlayer2() {
+        // TODO Auto-generated method stub
 
     }
 
