36b4db5df1c07fd91f63c091369c18a20fbc9b72
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-25 23:15:44 +0200

    Svuotato il main, dopo averlo usato per fare dei test.

--- src/it/unibo/squaresgameteam/squares/application/Application.java
+++ src/it/unibo/squaresgameteam/squares/application/Application.java
@@ -4,8 +4,11 @@ package it.unibo.squaresgameteam.squares.application;
 
 public class Application {  
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         
+       
+        
+       
         
     }
 }
