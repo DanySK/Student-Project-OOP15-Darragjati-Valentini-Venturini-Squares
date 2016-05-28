8527b1c2ec3ab980414913c7bb689e206767286c
Licia Valentini <licia.valentini@studio.unibo.it>
2016-05-28 15:56:58 +0200

    Rimossi alcuni import nel main che generavano errori.

--- src/it/unibo/squaresgameteam/squares/application/Application.java
+++ src/it/unibo/squaresgameteam/squares/application/Application.java
@@ -2,10 +2,7 @@ package it.unibo.squaresgameteam.squares.application;
 
 import java.io.IOException;
 
-import it.unibo.squaresgameteam.squares.controller.classes.SettingImpl;
-import it.unibo.squaresgameteam.squares.controller.classes.ShowRankingImpl;
-import it.unibo.squaresgameteam.squares.controller.interfaces.Setting;
-import it.unibo.squaresgameteam.squares.model.enumerations.RankingOption;
+
 import it.unibo.squaresgameteam.squares.model.exceptions.DuplicatedPlayerStatsException;
 
 
